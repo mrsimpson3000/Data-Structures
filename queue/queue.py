af34a031810b91e6bbdfc4191fcd5459727f65f9
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-07-24 12:09:09 -0400

    Queue.py completed and passing all tests.

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,57 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = []
+
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
 
     def dequeue(self):
-        pass
+        if len(self.storage) > 0:
+            return self.storage.pop(0)
+
+
+class Node:
+    def __init__(self, value, next_node):
+        self.value = value
+        self.next = next_node
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        new_node = Node(value, None)
+        if self.size == 0:
+            self.storage.head = new_node
+            self.storage.tail = new_node
+        else:
+            self.storage.tail.next = new_node
+            self.storage.tail = new_node
+        self.size += 1
+
+    def dequeue(self):
+        if self.size > 0:
+            popped_node = self.storage.head
+            self.storage.head = self.storage.head.next
+            self.size -= 1
+            return popped_node.value
